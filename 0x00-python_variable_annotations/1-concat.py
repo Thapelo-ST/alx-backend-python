a47bbf16a33d2637df2d13905dc04be689036364
Thapelo-ST <tsamkelo96@gmail.com>
2024-01-11 22:50:31 +0000

    first test

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    accepts and concatinates two strings and it is annotated.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+        accepts and concatinates two strings and it is annotated.
+    """
+    return str1 + str2
