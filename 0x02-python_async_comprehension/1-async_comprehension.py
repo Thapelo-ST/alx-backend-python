a62cac5a22799d9a57202b01303d0514bb49c0ba
Thapelo-ST <tsamkelo96@gmail.com>
2024-01-17 00:47:30 +0000

    Asyncronus Fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -10,7 +10,7 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """
-    executes async generator in a co routine called async_generator 
+    executes async generator in a co routine called async_generator
     without taking arguments
     """
     return [n async for n in async_generator()]
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,13 +8,13 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """runs async comprehesion parallel to async gather"""
     start_time = perf_counter()
-    
+
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
     )
-    
+
     end_time = perf_counter()
     return end_time - start_time
